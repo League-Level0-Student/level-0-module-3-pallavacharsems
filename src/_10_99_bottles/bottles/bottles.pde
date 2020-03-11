18470beece4d6f1b04d412cf52f5c53908e37bd5
pallavacharsems <56978876+pallavacharsems@users.noreply.github.com>
2020-03-11 16:28:10 -0700

    finished

--- src/_10_99_bottles/bottles/bottles.pde
+++ src/_10_99_bottles/bottles/bottles.pde
@@ -1,1 +1,7 @@
-size(400);
+
+for (int i = 99; i > 0; i--) {
+  println (i + " bottles of beer on the wall " + i+ " bottles of beer."
+  + " Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+}
+  
+  
--- src/_12_fizz_buzz/FizzBuzz.java
+++ src/_12_fizz_buzz/FizzBuzz.java
@@ -26,6 +26,47 @@ package _12_fizz_buzz;
 **/
 
 public class FizzBuzz {
-
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		for (int i=1; i<=20; i++) {
+			if (i % 3 == 0){
+		        System.out.println("Fizz");
+		    } 
+			    if (i % 5 == 0) {
+			        System.out.println("Buzz");
+		    
+			    
+			    
+			    }     
+			   
+			    if (i % 3 != 0 && i%5 != 0) {
+			    	System.out.println(i);
+			    	
+			    }
+		}
+		   
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		    
+		   
+		
+		
+		
+	}
 }
 
