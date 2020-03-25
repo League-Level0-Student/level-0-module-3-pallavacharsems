6df37a660f9c2f68d1a64bf5666ad15d3cb64ddf
Pallav <Pallav@DESKTOP-DVPQU53>
2020-03-25 15:43:31 -0700

    Finished Module 3

---
+++ src/_99_extra/Checkpoint3.java
@@ -0,0 +1,16 @@
+
+for (int i=1; i<=500; i--) {
+	if (i % 2 == 1){
+        System.out.println();
+    } 
+
+for (int i=1; i<=8; i++) {
+	
+if (i==4) {
+System.out.println("4");
+if (i==8);
+System.out.println("more");
+}
+else {
+	System.out.println(i + " potato ");
+}
